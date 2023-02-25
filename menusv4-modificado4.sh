795bb53e46d2a969b990347b4b6264fa41f51ad9
NEBULA1978 <rpascualey@gmail.com>
2023-02-25 15:03:34 +0100

    para que muestre un menú y permita al usuario elegir una técnica de apuestas y introducir una cantidad de dinero cada vez que se ejecuta una técnica

---
+++ menusv4-modificado4.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+function controlC(){
+    # Parametro -epara que me aplique los saltos de linea
+    echo -e "\n\n[!] Saliendo...\n"
+  # Codigo de estado no exitoso
+    exit 1  
+}
+
+# controlC
+# Para capturr el atajo de teclado que acabo de escribir y redirijirlo a la funcion controlC
+trap controlC INT
+
+# Si no ccomento se ejecuta despues de 10 segundos
+# sleep 10
+
+#///////////////////////////////////////////
+#///////////////////////////////////////////
+
+function helpPanel(){
+  echo -e "\n[!] Por favor, introduzca una cantidad de dinero y una técnica de apuestas válida. Las técnicas disponibles son martingala, d'Alembert y Paroli.\n"
+}
+
+while true; do
+  echo "Menú:"
+  echo "1. martingala"
+  echo "2. d'Alembert"
+  echo "3. Paroli"
+  echo "4. Salir"
+  read -p "Seleccione una técnica de apuestas (1-4): " technique_choice
+
+  if [ $technique_choice -eq 4 ]; then
+    echo -e "\n\n[!] Saliendo...\n"
+    exit 0
+  elif [ $technique_choice -ge 1 ] && [ $technique_choice -le 3 ]; then
+    read -p "Introduzca la cantidad de dinero: " money
+
+    if [ $money ]; then
+      case $technique_choice in
+        1) technique="martingala";;
+        2) technique="d'Alembert";;
+        3) technique="Paroli";;
+      esac
+      echo "Voy a jugar con $money dinero usando la tecnica $technique"
+    else
+      helpPanel
+    fi
+  else
+    helpPanel
+  fi
+done

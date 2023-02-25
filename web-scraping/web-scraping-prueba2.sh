38acf2ac52aebeba6a0aad5683471d4da8e83ea8
NEBULA1978 <rpascualey@gmail.com>
2023-02-25 17:00:30 +0100

    Corregido el error de los nombres y no ejecutar mas de una vez curl

--- web-scraping/web-scraping-prueba2.sh
+++ web-scraping/web-scraping-prueba2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# ><
+# Revisa si el archivo log.log ya existe y si es así, no ejecuta curl
+if [ ! -f log.log ]; then
+  # Vamos al explorador y copiamos la pagina que queramos
+  # Enviamos a un documento la peticion
+  # El output no queremos ver redigimos a dev/nul
+  curl https://www.vulnhub.com/ >log.log 2>/dev/null
+fi
 
-# Vamos al explorador y copiamos la pagina que queramos
-# Enviamos a un documento la peticion
-# El output no queremos ver redigimos a dev/nul
-curl https://www.vulnhub.com/ >log.log 2>/dev/null
-
-echo "Estas son las maquina disponibles: "
+echo "Estas son las maquinas disponibles: "
 
 # Inspeccionamos la etiqueta del contexto que queramos consultar
-# patron en comun a href=/entry/ de etiquetas html | tr elimino # | sed elimino un patron y pongo otra cosa | awk imprimeme la columna3 | unic no se repita nimguna fila
+# patron en comun a href=/entry/ de etiquetas html | tr elimino # | sed elimino un patron y pongo otra cosa | awk imprimeme la columna3 | sed elimino /entry/ del resultado | uniq no se repita ninguna fila
+grep -o 'href="/entry/[^"]*' log.log | sed 's/href="//' | awk -F ',' '{print $1}' | sed 's|/entry/||' | uniq >log.log2
 
-# echo "$(cat log.log | grep 'href="/entry/' | tr -d '#' | sed 's/entry/vm/' | awk '{print $3}' | tr -d '/' | sed 's/dowload/ /' | awk '{print $2}' | tr -d '>' | uniq)"
-# cat log.log | grep 'href="/entry/' | tr -d '#' | sed 's/entry/vm/' | awk '{print $3}' | tr -d '/' | sed 's/dowload/ /' | awk '{print $2}' | tr -d '>' | uniq
-# Mostrar comandos por consola
-# echo "cat log.log | grep 'href=/entry/' | tr -d '#' | sed 's/entry/ /' | awk '{print $3}' | tr -d '/' | sed 's/dowload/ /' | sed 's/ /' | tr -d '>'"
+# Imprime el contenido de log.log2
+cat log.log2
 
-# cat log.log | grep 'href=/entry/' | tr -d '#' | sed 's/entry/ /' | awk '{print }' | tr -d '/' | sed 's/dowload/ /' | sed 's/ /' | tr -d '>' | tr -d ' ' | uniq
+# Este script primero verifica si el archivo log.log ya existe, y si es así, no ejecuta el comando curl de nuevo. Luego, utiliza la expresión regular para extraer los nombres de las máquinas y los guarda en el archivo log.log2.
